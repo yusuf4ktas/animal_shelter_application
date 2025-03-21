69df12d5ce9e13f014a5393d2f9953a29d304bd8
Seher Sesli <150326598+sesliseher@users.noreply.github.com>
2025-03-21 23:52:41 +0300

    animal_image
    Animal foto yüklemek ve göstermek için method

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -1,5 +1,6 @@
 ﻿using animal_shelter_app.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace animal_shelter_app.Controllers
@@ -20,6 +21,43 @@ namespace animal_shelter_app.Controllers
                                   .Where(a => a.Species == species && !a.IsAdopted)
                                   .ToList();
             ViewBag.Species = char.ToUpper(species[0]) + species.Substring(1);
+            return View(animals);
+        }
+        // Hayvanların fotoğraflarını yüklemek için yazdığım method
+        /*Gerçek yönlendirmeyi yapmak için pathi düzgün vermemiz gerekiyor şu an o pathte bir hayvan bulamadığı
+        için Hayvan bulunamadı çıkıyor ekrana */
+        [HttpPost]
+        public IActionResult UploadImage(int id, IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                // Veritabanında güncelle
+                var animal = _context.AnimalInformations.Find(id);
+                if (animal != null)
+                {
+                    animal.AnimalImage = "/uploads/" + fileName;
+                    _context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        public async Task<IActionResult> Index()
+        {
+            // Veritabanından hayvanları çek
+            var animals = await _context.AnimalInformations.ToListAsync();
+
+            // View'a gönder
             return View(animals);
         }
 
