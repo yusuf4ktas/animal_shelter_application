297a405dc2fdd90cab9ea5919989cecf32701ae1
Seher Sesli <150326598+sesliseher@users.noreply.github.com>
2025-05-05 23:23:40 +0300

    Create a blog page

---
+++ Views/Article/Blog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace animal_shelter_app.Views.Article
+{
+    public class BlogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
